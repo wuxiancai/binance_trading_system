8417fec77a882a3a5ddb6fc7b6be20864c369f9b
poly-new <wuxiancai1978@gmail.com>
2025-09-16 22:00:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
     api_key: str = "00832d003a3d2f76c718ba02278363ec3be6abff352051e3c3b09ca0100b723f"                    # 币安 API Key（非干跑时必须）
     api_secret: str = "523755dec380c497f8bec63929ba16f3918a15bac06a78c6535ef65f0d5aa523"                 # 币安 API Secret
     use_testnet: bool = True              # 是否使用测试网：1/true 为测试网
-    symbol: str = "ETHUSDT"              # 交易对（如 BTCUSDT）
+    symbol: str = "BTCUSDT"              # 交易对（如 BTCUSDT）
     interval: str = "15m"               # K线周期（如 15m）
     window: int = 20                      # 布林带窗口期（默认 20）
 
